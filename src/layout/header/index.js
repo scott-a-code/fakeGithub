ddf8b2a7c7a6bdc6e5ff912058c0a4df67850605
Sammie <Lucidareverie@outlook.com>
2021-08-11 12:37:30 +0100

    remove navlink to user repos

--- src/layout/header/index.js
+++ src/layout/header/index.js
@@ -1,16 +1,10 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
-import { useParams } from 'react-router';
+import { NavLink, useHistory } from 'react-router-dom';
 import './style.css';
 import logo from './GitHub-Mark-Light-64px.png';
 
 const Header = () => {
 
-    const { userName } = useParams();
-    let userLink;
-
-    userName == undefined ? userLink = "/NotFound" : userLink = `/repos/${userName}`;
-    
     return (
         <header>
             <nav className="navbar">
@@ -22,7 +16,7 @@ const Header = () => {
                         <NavLink exact to="/" activeClassName="current">User search</NavLink>
                     </li>
                     <li className="nav-item">
-                        <NavLink exact to={userLink} activeClassName="current">Repos</NavLink>
+                        {/* <NavLink exact to={userLink} activeClassName="current">Repos</NavLink> */}
                     </li>
                 </ul>
             </nav>
