4ac1215e65424307b9651d7a46af0417933535f3
Sammie <Lucidareverie@outlook.com>
2021-08-11 10:26:56 +0100

    add css for nav bar

--- src/layout/header/index.js
+++ src/layout/header/index.js
@@ -1,14 +1,31 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { useParams } from 'react-router';
+import './style.css';
 
 const Header = () => {
+
+    const { userName } = useParams();
+    let userLink;
+
+    userName == undefined ? userLink = "NotFound" : userLink = `/repos/${userName}`;
+    
     return (
-        <nav>
-            <h1></h1>
-            <div className="links">
-                <NavLink exact to="/" activeClassName="current">User search</NavLink>
-            </div>
-        </nav>
+        <header>
+            <nav className="navbar">
+                <div id="logo">
+                    <img src="#" alt="Logo" /> 
+                </div>
+                <ul>
+                    <li className="Nav-item">
+                        <NavLink exact to="/" activeClassName="current">User search</NavLink>
+                    </li>
+                    <li className="Nav-item">
+                        <NavLink exact to={userLink} activeClassName="current">Repos</NavLink>
+                    </li>
+                </ul>
+            </nav>
+        </header>
     );
 }
 
